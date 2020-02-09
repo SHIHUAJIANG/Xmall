c457446d5917d098ea75a6fe7479f6d17c0de226
shihuajiang <s17824823374@gmail.com>
2020-02-09 16:47:37 +0800

    Extraction service-util

---
+++ xmall-service-util/src/main/java/top/flander/xmall/conf/RedisConfig.java
@@ -0,0 +1,29 @@
+package top.flander.xmall.conf;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import top.flander.xmall.util.RedisUtil;
+
+@Configuration
+public class RedisConfig {
+    //读取配置文件中的redis的ip地址
+    @Value("${spring.redis.host:disabled}")
+    private String host;
+    @Value("${spring.redis.port:0}")
+    private int port;
+    @Value("s18018994611")
+    private String pwd;
+    @Value("${spring.redis.database:0}")
+    private int database;
+
+    @Bean
+    public RedisUtil getRedisUtil() {
+        if (host.equals("disabled")) {
+            return null;
+        }
+        RedisUtil redisUtil = new RedisUtil();
+        redisUtil.initPool(host, port, database, pwd);
+        return redisUtil;
+    }
+}
---
+++ xmall-service-util/src/main/java/top/flander/xmall/conf/XmallRedissonConfig.java
@@ -0,0 +1,27 @@
+package top.flander.xmall.conf;
+
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class XmallRedissonConfig {
+
+    @Value("spring.redis.host:0")
+    private int host;
+    @Value("spring.redis.port:6379")
+    private int port;
+
+    @Bean
+    public RedissonClient redissonClient(){
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://"+host+":"+port);
+        RedissonClient redisson = Redisson.create(config);
+        return redisson;
+    }
+
+}
